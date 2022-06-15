33305dee05bb89c9ffbcbda89ddc136341d9f344
dauddi <kuntaytc@gmail.com>
2022-06-15 10:01:12 +0300

    subbed the app index page to redux store

--- pages/app/index.jsx
+++ pages/app/index.jsx
@@ -2,42 +2,58 @@ import React, {useState, useEffect} from "react";
 import { Container, Box, Stack, Flex, Spacer } from '@chakra-ui/react'
 import { Welcome,  InvoiceStats, RevenueStat } from "../../src/features/homepage"
 import { AppLayout } from "../../src/components";
-import Router from "next/router"
+import { useRouter } from "next/router"
 import axios from 'axios';
 import { authenticate, deauthenticate, addUser, clearUser } from "../../src/slices/authSlice";
 import { useSelector, useDispatch } from "react-redux";
+import nookies from 'nookies';
 
-const App = () => {
+const App = ({ userData }) => {
   const auth = useSelector(state => state.auth.isAuthenticated)
-  const { user } = useSelector(state => state.auth)
-  const dispatch = useDispatch();
+  // const { user } = useSelector(state => state.auth)
+  // const dispatch = useDispatch();
   const [isAuth, setIsAuth] = useState(auth);
+  const [user, setUser] = useState(null)
+  const router = useRouter();
 
   useEffect(() => {
-    if (!isAuth) {
-      Router.push('/app/auth/login')
-      return;
+    if(!user) {
+      return router.push('/app/auth/login')
     }
-  }, [isAuth])
+  }, [user])
 
-  return (
-      <Container maxW="container.xl" >
-        <Box w="100%" mt={{base: 2, md: 4}}>
-          <Stack spacing={{base: 6, md: 10}} >
-            <Welcome user={user} />
-            {/* <GetStarted /> */}
-            <Box bg='white' padding={10} boxShadow="sm" >
-              <Flex justify="space-between" >
-                <InvoiceStats />
-                <Spacer />
-                <RevenueStat />
-              </Flex>
-            </Box>
 
-          </Stack>
-        </Box>
-      </Container>
-  );
+  useEffect(() => {
+    setUser(userData);
+  }, [userData]);
+  // useEffect(() => {
+  //   if (!isAuth) {
+  //     Router.push('/app/auth/login')
+  //     return;
+  //   }
+  // }, [isAuth])
+  if (!user) {
+    router.push('/app/auth/login')
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+        <Container maxW="container.xl" >
+          <Box w="100%" mt={{base: 2, md: 4}}>
+            <Stack spacing={{base: 6, md: 10}} >
+              <Welcome user={user} />
+              {/* <GetStarted /> */}
+              <Box bg='white' padding={10} boxShadow="sm" >
+                <Flex justify="space-between" >
+                  <InvoiceStats />
+                  <Spacer />
+                  <RevenueStat />
+                </Flex>
+              </Box>
+            </Stack>
+          </Box>
+        </Container>
+    );
+  }
 }
 
 App.getLayout = function getLayout(page) {
@@ -48,21 +64,20 @@ App.getLayout = function getLayout(page) {
   )
 }
 
-// export const getServerSideProps = async (ctx) => {
-//   const { tkn: token } = nookies.get(ctx);
+export const getServerSideProps = async (ctx) => {
+  const { tkn: token } = nookies.get(ctx);
+  const { data: user } = await axios.get(`${process.env.STRAPI_API}/users/me`, {
+    headers: {
+      contentType: 'application/json',
+      Authorization: `Bearer ${token}`,
+    }
+  })
 
-//   const res = await axios.get(`${process.env.STRAPI_API}/users/me`, {
-//     headers: {
-//       contentType: 'application/json',
-//       Authorization: `Bearer ${token}`,
-//     }
-//   })
-
-//   console.log(res.status);
-
-//   return {
-//     props: res.status,
-//    }
-// }
+  return {
+    props: {
+      userData: user,
+    },
+   }
+}
 
 export default App;

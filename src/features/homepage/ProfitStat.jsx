54110ad3f8e65a2933a6fc50baf359d018eafec7
dauddi <kuntaytc@gmail.com>
2022-06-15 09:58:49 +0300

    intial profit stat card

---
+++ src/features/homepage/ProfitStat.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Box, Select } from '@chakra-ui/react'
+
+const ProfitStat = () => {
+  return (
+    <Box>
+      <Heading>Total Profit</Heading>
+
+    </Box>
+  );
+};
+
+export default ProfitStat;
